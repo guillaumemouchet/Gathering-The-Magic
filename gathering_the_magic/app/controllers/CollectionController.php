8d89945cf334336a749bc56d0870285614df0537
benjamin.mouchet <benjamin.mouchet@he-arc.ch>
2022-04-24 15:42:33 +0200

    Issue 16: possibility to remove card from collection

--- gathering_the_magic/app/controllers/CollectionController.php
+++ gathering_the_magic/app/controllers/CollectionController.php
@@ -73,44 +73,56 @@ class CollectionController
             }
         }
     }
-
-    public function parseRemoveCard()
+    public static function parseUpdateCard()
     {
-       //TODO REMOVE CARD
-       if($_SERVER['REQUEST_METHOD'] === 'POST')
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
         {
-            //New condition needed when user is implemented
-            if(isset($_POST['card_id']))
+            $qty = Collection::fetchQuantity($_POST['card_id'], $_POST['user_id'], $_POST['owned']);
+            $qty = $qty['quantity'] + $_POST['quantity'];
+            if($qty < 0)
             {
-                $collection = new Collection;
-                $collection->setCardId($_POST['card_id']);
-                if(isset($_POST['possession']))
-                {
-                    if($_POST['possession'] == "owned")
-                    {
-                        $collection->setOwned(true);
-                    }                    
-                    else
-                    {
-                        $collection->setOwned(false);
-                    }
-                }
-                if(isset($_POST['quantity']))
-                {
-                    $collection->setQuantity($_POST['quantity']);
-                }
-                $collection->setUserId(1);
-                // TODO THE DELETE $collection->delete();
+                CollectionController::parseRemoveCard();
+                exit();
+            }
+            if(isset($_POST['quantity']))
+            {
+                $params = [
+                    "binding" => [
+                        "quantity" => [$qty, PDO::PARAM_INT],
+                        "card_id" => [$_POST['card_id'], PDO::PARAM_INT],
+                        "user_id" => [$_POST['user_id'], PDO::PARAM_INT],
+                        "owned" => [$_POST['owned'], PDO::PARAM_STR]
+                    ]
+                ];
+                Collection::updateQuantity($params);
                 $collection = Collection::fetchAll(1);
                 return Helper::view("Collection", ["collection" => $collection]);
                 exit();
             }
-            else
-            {
-                $collection = Collection::fetchAll(1);
-                return Helper::view("Collection", ["collection" => $collection]);
-                exit();
-            }
+        }
+    }
+    public static function parseRemoveCard()
+    {
+        if($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            $params = [
+                "binding" => [
+                    "card_id" => [$_POST['card_id'], PDO::PARAM_INT],
+                    "user_id" => [$_POST['user_id'], PDO::PARAM_INT],
+                    "owned" => [$_POST['owned'], PDO::PARAM_STR]
+                ]
+            ];
+            Collection::remove($params);
+            $collection = Collection::fetchAll(1);
+            return Helper::view("Collection", ["collection" => $collection]);
+            exit();
+        }
+        else
+        {
+            $collection = Collection::fetchAll(1);
+            return Helper::view("Collection", ["collection" => $collection]);
+            exit();
         }
     }
     public function show()
--- gathering_the_magic/app/views/ShowCard.view.php
+++ gathering_the_magic/app/views/ShowCard.view.php
@@ -9,26 +9,13 @@
 				<img src="public/images/Card.jpg" alt="Blank Card"/>
 				<hr>
 <form method="post" action="parse_add_card">
-    <label>Quantity: <input type="range" id="quantity" name="quantity" min="1" max="10" step="1" list="tickmarks"/></label>
-    <!-- Option not fully supported https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range  -->
-    <!-- Firefox does not display ticks, will change quantity input in future -->
-    <datalist id="tickmarks">
-        <option value="1" label="1"></option>
-        <option value="2"></option>
-        <option value="3"></option>
-        <option value="4"></option>
-        <option value="5" label="5"></option>
-        <option value="6"></option>
-        <option value="7"></option>
-        <option value="8"></option>
-        <option value="9"></option>
-        <option value="10" label="10"></option>
-    </datalist>
+    <label>Quantity (between 1 and 100) <input type="number" id="quantity" name="quantity" min="1" max="100"></label>
     <label>
         <label>Owned:<input type="radio" id="owned" name="possession" value="owned" checked></label>
         <label>Wishlist:<input type="radio" id="wishlist" name="possession" value="wishlist"></label>
     </label>
     <input type="hidden" id="card_id" name="card_id" value="<?php echo $card[0]->getId();?>">
+    <input type="hidden" id="user_id" name="user_id" value="1">
     <input type="submit" name="add" value="Add to Collection !"/>
 </form>
 				
