57d7a8fbbd7e72ee18d799af188c97459cd0ceb3
Guillaume Mouchet <guillaume.mouchet@he-arc.ch>
2022-06-08 21:10:21 +0200

    Correction du nav

--- gathering_the_magic/app/views/ShowCard.view.php
+++ gathering_the_magic/app/views/ShowCard.view.php
@@ -5,22 +5,19 @@ Helper::checkLogin();
 ?>
 <h1><?php echo $card[0]->getName(); ?></h1>
 
-<?php
-    if(isset($_SESSION['message']))
-    {
-        echo $_SESSION['message'];
-        unset($_SESSION['message']);
-    }
-?>
-
 <?php echo $card[0]->asHTMLFlexBoxItem(); ?>
 <hr>
 <form class="form_display" method="post" action="parse_add_card">
     <label>How many cards do you want to add (between 1 and 100) <input type="number" id="quantity" name="quantity" min="1" max="100" value="1"></label>
-    <label>
-        <label>Owned:<input type="radio" id="owned" name="possession" value="owned" checked></label>
-        <label>Wishlist:<input type="radio" id="wishlist" name="possession" value="wishlist"></label>
-    </label>
+    <div class="mb form-check d-flex row">
+        <div class="mx-3">
+            <label>Owned: <input type="radio" id="owned" name="possession" value="owned" checked></label>
+        </div>
+        <div class="mx-3">
+
+            <label>Wishlist: <input type="radio" id="wishlist" name="possession" value="wishlist"></label>
+        </div>
+    </div>
     <input type="hidden" id="card_id" name="card_id" value="<?php echo $card[0]->getId(); ?>">
 
     <label id="btnSubmit"><input type="submit" name="add" value="Add to Collection !" /></label>
